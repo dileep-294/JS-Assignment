dd9255a79733f89451f0f72e7e021627fc80d8af
Dileep <dileep.jallipalli@pramati.com>
2019-12-23 09:39:40 +0530

    Added testing file

---
+++ index1.test.js
@@ -0,0 +1,20 @@
+const { addImage,addName } = require('./script');
+
+// test('DOM is setup', function () {
+   
+//     addPerson("jallipalli","dileep","francisca.sanchez@example.com","Mr");
+//     expect(document.getElementsByClassName('container').length).toEqual(1);
+// });
+test('check Image',function(){
+    var y=document.getElementsByClassName('logo');
+    expect(y).toBeTruthy();
+});
+test('check Name',function(){
+    var x= addName("jalli[palli","Dilep","MR");
+    var y=document.getElementsByClassName('spanE');
+    expect(y.length).toEqual(0);
+});
+//  test('check Name',function(){
+//      var y=document.getElementsByClassName('spanE');
+//      expect(y.length).toEqual(1);
+//  });
--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-const app=document.getElementById("root");
 
-function Loadmore(){
-var request=new XMLHttpRequest();
-url = 'https://randomuser.me/api/?results=100';
-request.open('GET',url,true);
-request.onload=function(){
-    var data=JSON.parse(this.response);
-    var data2=data['results']
-    create_UI(data2);
-} 
-request.send();  
+
+const app = document.createElement('div');
+app.setAttribute('id', 'root');
+document.body.appendChild(app);
+function Loadmore() {
+    var request = new XMLHttpRequest();
+    url = 'https://randomuser.me/api/?results=100';
+    request.open('GET', url, true);
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        var data2 = data['results']
+        console.log(data2)
+        for (var i = 0; i < data2.length; i++) {
+            const container = document.createElement('div');
+            container.setAttribute('class', 'container');
+            //const localdata = data2[i];
+            //console.log(localdata.name.first);
+            //var img=data2[i].picture.large;
+            //ar lname=data2[i].name.last;
+            // var fname=data2[i].name.first;
+            // var ntitle=data2[i].name.title;
+            // addPerson(fname,lname,img,ntitle)
+            var x=addImage(data2[i].picture.large)
+            var y=addName(data2[i].name.last,data2[i].name.first,data2[i].name.title)
+            container.appendChild(x);
+            container.appendChild(y);
+            app.appendChild(container);
+
+        }
+    }
+    request.send();
 }
-window.onscroll=function(){
-    if((window.innerHeight+window.scrollY)>=document.body.offsetHeight){
+
+window.onscroll = function () {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         this.onload();
     }
 }
+addImage=function(img_data){
+    const logo = document.createElement('img');
+     logo.src = img_data;
+     logo.setAttribute('class','logo');
+    return logo;
+}
+addName=function(f,l,t){
+    const spanE=document.createElement('div');
+    spanE.innerText=t+"."+" "+f+ " "+l;
+    spanE.setAttribute('class','spanE');
+    return spanE;
+}
+// addPerson = function (fname,lname,img,ntitle) {
+//     const container = document.createElement('div');
+//     container.setAttribute('class', 'container');
+//     if (img) {
+//         const logo = document.createElement('img');
+//         logo.src = img;
+//         logo.setAttribute('class', 'logo');
+//         container.appendChild(logo);
+//     }
+//     if (fname && lname && ntitle) {
+//         const spanE = document.createElement('div');
+//         spanE.innerText = ntitle+ "." + " " + fname + " " + lname;
+//         spanE.setAttribute('class', 'spanE');
+//         container.appendChild(spanE);
+//     }
+    
+//         app.appendChild(container);
+//     }
+    module.exports={addImage,addName}
