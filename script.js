5e2783c63c39d25ea256b9a798ca9d0e8a0cdde2
Dileep <dileep.jallipalli@pramati.com>
2020-01-08 10:26:12 +0530

    added Test case

--- script.js
+++ script.js
@@ -14,13 +14,6 @@ function Loadmore() {
         for (var i = 0; i < data2.length; i++) {
             const container = document.createElement('div');
             container.setAttribute('class', 'container');
-            //const localdata = data2[i];
-            //console.log(localdata.name.first);
-            //var img=data2[i].picture.large;
-            //ar lname=data2[i].name.last;
-            // var fname=data2[i].name.first;
-            // var ntitle=data2[i].name.title;
-            // addPerson(fname,lname,img,ntitle)
             var x=addImage(data2[i].picture.large)
             var y=addName(data2[i].name.last,data2[i].name.first,data2[i].name.title)
             container.appendChild(x);
@@ -49,22 +42,5 @@ addName=function(f,l,t){
     spanE.setAttribute('class','spanE');
     return spanE;
 }
-// addPerson = function (fname,lname,img,ntitle) {
-//     const container = document.createElement('div');
-//     container.setAttribute('class', 'container');
-//     if (img) {
-//         const logo = document.createElement('img');
-//         logo.src = img;
-//         logo.setAttribute('class', 'logo');
-//         container.appendChild(logo);
-//     }
-//     if (fname && lname && ntitle) {
-//         const spanE = document.createElement('div');
-//         spanE.innerText = ntitle+ "." + " " + fname + " " + lname;
-//         spanE.setAttribute('class', 'spanE');
-//         container.appendChild(spanE);
-//     }
-    
-//         app.appendChild(container);
-//     }
+
     module.exports={addImage,addName}
